ed491db403535274d85a88e1280a50f1a731f8d4
DevMohamedGaber <63926336+DevMohamedGaber@users.noreply.github.com>
2023-11-28 23:23:49 +0200

    Adding Login UI

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,7 +1,46 @@
 <template>
-  login
+  <v-container fill-height fluid class="x-ctn">
+    <v-row class="h-screen" align="center" justify="space-around">
+        <v-card width="35%" min-width="300" elevation="4" style="padding: 10px;">
+
+          <v-card-item>
+            <v-img src="@/assets/logo.png" height="150"/>
+          </v-card-item>
+
+          <v-card-item>
+            <v-form>
+              <v-text-field
+                name="username"
+                label="Username"
+                v-model="username"
+              ></v-text-field>
+
+              <v-text-field
+                name="password"
+                label="Password"
+                v-model="password"
+                type="password"
+              ></v-text-field>
+            </v-form>
+          </v-card-item>
+          <div class="mb-3" align="center">
+            <v-btn class="center" width="100" color="primary" variant="elevated" @clicl="Login">
+              Login
+            </v-btn>
+          </div>
+        </v-card>
+    </v-row>
+  </v-container>
 </template>
 
 <script lang="ts" setup>
-  
+  import {ref} from 'vue'
+
+  const username = ref("");
+  const password = ref("");
+
+  function Login() {
+    console.log(username.value);
+    
+  }
 </script>
